dd0f6e9868ffa74fa22b9847f0783a5f7d411d7b
hanxiao <artex.xh@gmail.com>
2015-08-01 11:58:15 +0200

    test

--- examples/throw-confidence/throw-confidence.js
+++ examples/throw-confidence/throw-confidence.js
@@ -1,28 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var stack = gajus.Swing.Stack(),
-        cardElement = document.querySelector('.stack li'),
-        throwOutConfidenceBind = document.querySelector('#throw-out-confidence-bind'),
-        directionBind = document.querySelector('#direction-bind'),
-        throwOutConfidenceElements = {};
+document.addEventListener('DOMContentLoaded', addCard);
 
-    window.card = stack.createCard(cardElement);
+function addCard() {
+	var stack = gajus.Swing.Stack(),
+	cardElement = document.querySelector('.stack li'),
+	throwOutConfidenceBind = document.querySelector('#throw-out-confidence-bind'),
+	directionBind = document.querySelector('#direction-bind'),
+	throwOutConfidenceElements = {},
+	throwOutConfidence = 0;
+	
+	window.card = stack.createCard(cardElement);
 
-    stack.on('dragstart', function (e) {
-        throwOutConfidenceElements.yes = e.target.querySelector('.yes').style;
-        throwOutConfidenceElements.no = e.target.querySelector('.no').style;
-    });
+	stack.on('dragstart', function (e) {
+		throwOutConfidenceElements.yes = e.target.querySelector('.yes').style;
+		throwOutConfidenceElements.no = e.target.querySelector('.no').style;
+	});
 
-    stack.on('dragmove', function (e) {
-        throwOutConfidenceElements[e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT ? 'yes' : 'no'].opacity = e.throwOutConfidence;
+	stack.on('dragmove', function (e) {
+		throwOutConfidenceElements[e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT ? 'yes' : 'no'].opacity = e.throwOutConfidence * 0.5;
+		throwOutConfidence =  e.throwOutConfidence;
+		throwOutConfidenceBind.innerHTML = e.throwOutConfidence.toFixed(3);
+		directionBind.innerHTML = e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT ? 'right' : 'left';
+	});
 
-        throwOutConfidenceBind.innerHTML = e.throwOutConfidence.toFixed(3);
-        directionBind.innerHTML = e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT ? 'right' : 'left';
-    });
-
-    stack.on('dragend', function (e) {
-        if (e.throwOutConfidence != 1) {
-            throwOutConfidenceElements.yes.opacity = 0;
-            throwOutConfidenceElements.no.opacity = 0;
-        }
-    });
-});
+	stack.on('dragend', function (e) {
+		if (throwOutConfidence != 1) {
+			throwOutConfidenceElements.yes.opacity = 0;
+			throwOutConfidenceElements.no.opacity = 0;
+		} else {
+			window.card.destroy()
+		}
+	});
+}
